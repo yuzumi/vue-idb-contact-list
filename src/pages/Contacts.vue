9e494eec5263f7a16119623506e867a8cd93277c
Bogdan <bogdan7morozyuk@gmail.com>
2020-05-09 13:22:08 +0300

    feat: display contacts on the home page

--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -1,5 +1,24 @@
 <template>
   <div class="contacts">
-    Home page
+    <contact-list :contacts="contacts" v-if="contacts.length" />
+    <no-contact v-else />
   </div>
 </template>
+
+<script>
+import { contacts } from '@/store'
+
+import NoContact from '@/components/contacts/NoContact'
+import ContactList from '@/components/contacts/ContactList'
+
+export default {
+  name: 'Contacts',
+  computed: {
+    ...contacts.mapGetters(['contacts'])
+  },
+  components: {
+    NoContact,
+    ContactList
+  }
+}
+</script>
