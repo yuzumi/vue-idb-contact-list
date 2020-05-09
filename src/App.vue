94d396304e516ba389bd8b5aaf38c23c9ad5d8ca
Bogdan <bogdan7morozyuk@gmail.com>
2020-05-09 12:51:36 +0300

    chore: delete all unnecessary things

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div class="app" id="app">
     <mdb-container>
       <mdb-row>
         <mdb-col>
@@ -9,26 +9,3 @@
     </mdb-container>
   </div>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
