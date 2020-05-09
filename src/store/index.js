c57b0ddf13bc16f20b748bede5ac7cf2aaff9da7
Bogdan <bogdan7morozyuk@gmail.com>
2020-05-09 13:18:47 +0300

    chore(store): add auxiliary functions for the contact module

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
+import Vuex, { createNamespacedHelpers } from 'vuex'
 
 import modules from '@/store/modules'
 import plugins from '@/store/plugins'
 
 Vue.use(Vuex)
+
+export const contacts = createNamespacedHelpers('contacts')
 
 export default new Vuex.Store({
   modules,
