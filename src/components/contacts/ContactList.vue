71777f5450c880ca10ecb8fcd5bf8582db0805b0
Bogdan <bogdan7morozyuk@gmail.com>
2020-05-09 13:21:34 +0300

    feat: add components to display contacts

---
+++ src/components/contacts/ContactList.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="contact-list">
+    <mdb-row v-for="contact of contacts" :key="contact.id">
+      <mdb-col>
+        <contact-list-item :contact="contact" />
+      </mdb-col>
+    </mdb-row>
+  </section>
+</template>
+
+<script>
+import ContactListItem from '@/components/contacts/ContactListItem'
+
+export default {
+  name: 'ContactList',
+  props: {
+    contacts: {
+      type: Array,
+      default: () => []
+    }
+  },
+  components: {
+    ContactListItem
+  }
+}
+</script>
---
+++ src/components/contacts/ContactListItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <mdb-card class="contact-list-item">
+    <mdb-card-body>
+      <mdb-card-title>{{ contact.name }}</mdb-card-title>
+      <mdb-card-text>email: {{ contact.email }}</mdb-card-text>
+      <mdb-card-text>phone: {{ contact.phone }}</mdb-card-text>
+    </mdb-card-body>
+  </mdb-card>
+</template>
+
+<script>
+export default {
+  name: 'ContactListItem',
+  props: {
+    contact: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
